1984f1d2199840560a504b4b6981b0dbc406d9ce
Dr. Smith <tcsmith@ut.edu>
2019-05-17 19:10:58 -0400

    add c05ex02.py

---
+++ Class05/Exercise_Solutions/c05ex2.py
@@ -0,0 +1,23 @@
+import random 
+
+e2f = {1:"un", 
+    2:"deux",
+    3:"trois",
+    4:"quatre",
+    5:"cinq",
+    6:"six",
+    7:"sept",
+    8:"huit",
+    9:"neuf"}
+
+print("Welcome to the french numbers game.")
+num = random.randint(1,9)
+while True:
+    ans = input("What is the french word for " + str(num) + " (print exit to exit): ")
+    if ans == e2f[num]:
+        print("Congratulations")
+    elif ans.lower() == ans:
+        break
+    else:
+        print("Sorry, the french word for", num, "is", e2f[num])
+print("Thank you for playing!")
